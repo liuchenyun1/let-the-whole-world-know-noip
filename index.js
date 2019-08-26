858de26c53601e40baffb44541e41e1d2587b128
sjx233 <sjx233@qq.com>
2019-08-26 11:51:00 +0800

    Modules

--- index.js
+++ index.js
@@ -1,43 +1,19 @@
-export default async function start(interval) {
-  const refresh = async () => {
-    const res = await fetch("http://www.noi.cn/GetNews.dt", {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      body: "cmd=read&newsid=932&hash=72F731",
-      method: "POST"
-    });
-    const obj = await res.json();
-    return Number(obj.click);
-  };
+export default async function start(interval, { refresh, init, update }) {
   let lastCount = await refresh();
   let lastTime = performance.now();
   let initCount;
   let initTime;
-  const newRow = () => {
-    const elem = document.createElement("small");
-    elem.classList.add("news-click");
-    return elem;
-  };
-  const rowsElem = document.getElementsByClassName("maintitle")[0];
-  rowsElem.appendChild(newRow());
-  rowsElem.appendChild(newRow());
-  rowsElem.appendChild(newRow());
-  const [countElem, speedElem, avgElem, recomputeElem] = document.getElementsByClassName("news-click");
-  const recomputeButton = document.createElement("button");
-  recomputeButton.innerText = "重新计算平均速度";
-  (recomputeButton.onclick = () => {
+  const recomputeAverage = () => {
     initCount = lastCount;
     initTime = lastTime;
-  })();
-  recomputeElem.appendChild(recomputeButton);
+  };
+  recomputeAverage();
+  if (init instanceof Function) init = init(recomputeAverage);
   return setInterval(async () => {
     const count = await refresh();
     const time = performance.now();
     if (count < lastCount || time < lastTime) return;
-    countElem.innerText = `阅读量：${count}`;
-    speedElem.innerText = `当前速度：${((count - lastCount) / ((time - lastTime) * 0.001)).toFixed(3)} 次/s`;
-    avgElem.innerText = `平均速度：${((count - initCount) / ((time - initTime) * 0.001)).toFixed(3)} 次/s`;
+    if (update) update(count, (count - lastCount) / (time - lastTime), (count - initCount) / (time - initTime), init);
     lastCount = count;
     lastTime = time;
   }, interval);
---
+++ noi.js
@@ -0,0 +1,33 @@
+export async function refresh() {
+  return Number((await (await fetch("http://www.noi.cn/GetNews.dt", {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "cmd=read&newsid=932&hash=72F731",
+    method: "POST"
+  })).json()).click);
+}
+
+export function init(recomputeAverage) {
+  const newRow = () => {
+    const elem = document.createElement("small");
+    elem.classList.add("news-click");
+    return elem;
+  };
+  const rowsElem = document.getElementsByClassName("maintitle")[0];
+  rowsElem.appendChild(newRow());
+  rowsElem.appendChild(newRow());
+  rowsElem.appendChild(newRow());
+  const [countElem, speedElem, averageElem, recomputeElem] = document.getElementsByClassName("news-click");
+  const recomputeButton = document.createElement("button");
+  recomputeButton.innerText = "重新计算平均速度";
+  recomputeButton.onclick = recomputeAverage;
+  recomputeElem.appendChild(recomputeButton);
+  return [countElem, speedElem, averageElem];
+}
+
+export function update(count, speed, average, [countElem, speedElem, averageElem]) {
+  countElem.innerText = `阅读量：${count}`;
+  speedElem.innerText = `当前速度：${(speed * 1000).toFixed(3)} 次/s`;
+  averageElem.innerText = `平均速度：${(average * 1000).toFixed(3)} 次/s`;
+}
